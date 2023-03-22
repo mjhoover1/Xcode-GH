cc61d817762135f9c460956bb5db3bf1bd0788d8
Montana Hoover <fctanny@yahoo.com>
2023-03-22 08:07:38 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Montana Hoover on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
